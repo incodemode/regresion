1604b46aa17530657f4e25e587fdefe320af1610
mau <lmdbluis@gmail.com>
2018-08-30 07:19:19 -0600

    fixing opera

--- public/assets/js/bootstrap.operamini.fix.js
+++ public/assets/js/bootstrap.operamini.fix.js
@@ -1,14 +1,17 @@
-if (typeof operamini != 'undefined') {  //check to see if operamini a JS var added by Opera Mini browser exists so other browsers won't error
-    if(operamini) { //this should only work for Opera Mini
-      $(document).on("click", '#menuButton', function(e){ //bind click which Opera Mini likes better
-        e.preventDefault(); //prevent default action
-        $(this).collapse({ //manually add collapse to the targeted button
-          toggle:true
-        });
-      });
-      $(document).on("submit", "form", function(evt){
-      	evt.preventDefaul();
-      	return false;
-      });
-    }
-  }
+$(function(){
+	if (typeof operamini != 'undefined') {  //check to see if operamini a JS var added by Opera Mini browser exists so other browsers won't error
+	    if(operamini) { //this should only work for Opera Mini
+	      $(document).on("click", '#menuButton', function(e){ //bind click which Opera Mini likes better
+	        e.preventDefault(); //prevent default action
+	        $(this).collapse({ //manually add collapse to the targeted button
+	          toggle:true
+	        });
+	      });
+	      $(document).on("submit", "form", function(evt){
+	      	evt.preventDefaul();
+	      	return false;
+	      });
+	      alert("operamini");
+	    }
+	}
+});
