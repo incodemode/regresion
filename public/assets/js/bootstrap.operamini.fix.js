0ec257c33ebd13d0b4ed7581644a98ba7f4bfdf5
mau <lmdbluis@gmail.com>
2018-08-30 07:46:29 -0600

    fixing opera

--- public/assets/js/bootstrap.operamini.fix.js
+++ public/assets/js/bootstrap.operamini.fix.js
@@ -2,7 +2,7 @@ $(function(){
 	alert("llega a ready");
 
 	function isOperaMini(){
-		return navigator.userAgent.match(/Opera Mini/i);
+		return (navigator.userAgent.indexOf('Opera Mini') > -1);
 	};
 	/*if (window.opera) {
 	    alert(opera.version()); 
