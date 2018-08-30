09f72fad58ca440bc8ec98feb8a7204568a6fa85
mau <lmdbluis@gmail.com>
2018-08-30 07:41:46 -0600

    fixing opera

--- public/assets/js/bootstrap.operamini.fix.js
+++ public/assets/js/bootstrap.operamini.fix.js
@@ -1,9 +1,14 @@
 $(function(){
 	alert("llega a ready");
+
+
+	if (window.opera) {
+	    alert(opera.version()); 
+	}
 	if (typeof operamini != 'undefined') {  //check to see if operamini a JS var added by Opera Mini browser exists so other browsers won't error
 	    if(operamini){ //this should only work for Opera Mini
 	      alert("operamini");
-	      $(document).on("click", '#menuButton', function(e){ //bind click which Opera Mini likes better
+	      $(document).on("click", '#useExample', function(e){ //bind click which Opera Mini likes better
 	        e.preventDefault(); //prevent default action
 	        $(this).collapse({ //manually add collapse to the targeted button
 	          toggle:true
